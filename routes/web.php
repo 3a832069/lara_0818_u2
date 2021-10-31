393f7839b3ddcfc0a6fca884ca9a196a3d36375a
Len-En Chiu <linda34264622@gmail.com>
2021-10-31 12:38:00 +0800

    改用用 HomeController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('home',function(){
+/*Route::get('home',function(){
     return view('home.index');
-});
-
+});*/
+Route::get('home', [HomeController::class, 'index']);
